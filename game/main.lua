1357566e0bf3d038b7dbe1b6d06b13f329471f12
Lucas Oshiro <lucasseikioshiro@gmail.com>
2018-02-13 16:46:33 -0200

    Window title and icon

--- game/main.lua
+++ game/main.lua
@@ -1,14 +1,16 @@
 function love.load()
-    love.window.setMode(400, 700)
-    W, H = love.graphics.getDimensions()
-    ifFontLarge = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/6)
-    ifFontSmall = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/11)
-    ifFontVerySmall = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/22)
-    ifFontTiny = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/25)
-    sound = true
+   love.window.setMode(400, 700)
+   love.window.setTitle 'AnotherBrick'
+   love.window.setIcon(love.image.newImageData "Assets/images/icon.png")
+   W, H = love.graphics.getDimensions()
+   ifFontLarge = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/6)
+   ifFontSmall = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/11)
+   ifFontVerySmall = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/22)
+   ifFontTiny = love.graphics.newFont("Assets/Font/IndieFlower/IndieFlower.ttf", W/25)
+   sound = true
 
-    ScreenManager = require 'ScreenManager'
-    math.randomseed (os.time())
-    math.random () -- descarta o primeiro valor aleatório
-    ScreenManager.changeTo 'Menu_Screen'
+   ScreenManager = require 'ScreenManager'
+   math.randomseed(os.time())
+   math.random() -- descarta o primeiro valor aleatório
+   ScreenManager.changeTo 'Menu_Screen'
 end
